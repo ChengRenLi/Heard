0dc0356646a2e24238def7efd609b5a67d3dde1f
chengren.li <18060383800@163.com>
2017-07-20 00:29:04 +0800

    这是我修改的

--- heard/src/main/java/com/example/custom/heard/activity/Person.java
+++ heard/src/main/java/com/example/custom/heard/activity/Person.java
@@ -12,4 +12,20 @@ public class Person {
         this.width = width;
         this.height = height;
     }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
 }
