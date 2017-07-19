0a008298895751f26063e99998735731edff53fd
chengren.li <18060383800@163.com>
2017-07-20 00:26:32 +0800

    this is tag

---
+++ heard/src/main/java/com/example/custom/heard/activity/Person.java
@@ -0,0 +1,15 @@
+package com.example.custom.heard.activity;
+
+/**
+ * Created by lichengren on 17/7/20.
+ */
+
+public class Person {
+    int width;
+    int height;
+
+    Person(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+}
